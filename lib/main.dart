2fce0798931a2e01ad8ebc74120079832ae0882a
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 19:41:43 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
-main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  //runApp serve para chamar widgets, container é um dos widgets padroes do flutter
+  runApp(Container(
+    child: Text('Teste',
+    textDirection: TextDirection.ltr,
+    style: TextStyle(color: Colors.white,
+    fontSize: 50.0,),
+    ),
+
+  ));
+  
+}
