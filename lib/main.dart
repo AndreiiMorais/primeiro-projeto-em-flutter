664d14a6bfff4fa20a7ec8cdea62061437cf8fb6
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 23:32:08 -0300

    criada primeira tela no flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 main() {
   //runApp serve para chamar widgets, container é um dos widgets padroes do flutter
   runApp(Container(
-    child: Text('Teste',
-    textDirection: TextDirection.ltr,
-    style: TextStyle(color: Colors.white,
-    fontSize: 50.0,),
+    child: const Center(
+      //o center faz a centralização do texto.
+      child: Text(
+        'Teste',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 50.0),
+      ),
     ),
-
   ));
-  
 }
