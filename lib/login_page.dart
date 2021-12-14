1aa2499d1fb56d7f7142dd575fb4d14d919b8ee2
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-14 15:14:05 -0300

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -59,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
                     }
                   },
                   child: const Text('Entrar'),
-                )
+                ),
               ],
             ),
           ),
