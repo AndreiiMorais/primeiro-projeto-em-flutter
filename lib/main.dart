189755cd9388f9b597b97c73eba732b0fdfa0374
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 23:35:38 -0300

    criada a class AppWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,22 @@ import 'package:flutter/material.dart';
 
 main() {
   //runApp serve para chamar widgets, container é um dos widgets padroes do flutter
-  runApp(Container(
-    child: const Center(
-      //o center faz a centralização do texto.
-      child: Text(
-        'Teste',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 50.0),
+  runApp(AppWidget());
+  //dessa forma o runapp esta apenas chamando a outra classe
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        //o center faz a centralização do texto.
+        child: Text(
+          'Teste',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
