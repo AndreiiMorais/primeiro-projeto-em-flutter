ba31dd70ededde87bf03ec88c4a3694fc304ef3f
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-14 12:01:45 -0300

    colunas e alinhamento de colunas

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,18 +15,30 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       //scaffold é um widget que tem toda a parte visual "meio pronta"
       appBar: AppBar(
-        title: Center(
-          child: Text('Home Page'),
-        ),
+        title: Text('Home Page'),
+        actions: [
+          CustomSwitch(),
+        ],
       ),
 
-      body: Align(
-        alignment: Alignment.topRight,
-        child: Switch(
-            value: AppController.instance.isDarkTheme,
-            onChanged: (value) {
-              AppController.instance.changeTheme();
-            }),
+      body: Container(
+        //para fazer alinhamento da coluna é necessario herdar do pai, no caso o container
+        width: double.infinity,
+        height: double.infinity,
+        //a opçao double.infinity coloca o container no tamanho maximo disponivel
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //faz o alinhamento principal no centro
+          //crossAxisAlignment: CrossAxisAlignment.end,
+          // o cross comentado faz o alinhamento para os lados ou de tamanho
+          children: [
+            Text(
+              'Contador: $counter',
+              style: TextStyle(fontSize: 40, color: Colors.black),
+            ),
+            CustomSwitch(),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_box_outlined),
@@ -37,5 +49,18 @@ class HomePageState extends State<HomePage> {
         },
       ),
     );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: AppController.instance.isDarkTheme,
+        onChanged: (value) {
+          AppController.instance.changeTheme();
+        });
   }
 }
