795c2138e7f3991b8608eb57dd338d4336c64831
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 23:42:41 -0300

    adicionado o title
    foi modificado a forma do text e passado o titulo por parametro

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 
 main() {
   //runApp serve para chamar widgets, container é um dos widgets padroes do flutter
-  runApp(AppWidget());
+  runApp(const AppWidget(title: 'Andrei',));
   //dessa forma o runapp esta apenas chamando a outra classe
 }
 
 class AppWidget extends StatelessWidget {
+  //stateless é um widget q nao se altera, nao tem animaçao, imutavel, sem estado
+  final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+  
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(
+      child: Center(
         //o center faz a centralização do texto.
         child: Text(
-          'Teste',
+          title,
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 50.0),
         ),
