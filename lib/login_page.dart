8cf1692063a520ec60f411d3118a17e4878e0004
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-14 17:51:47 -0300

    adicionado background
    modificado as caixas de texto e adicionado background

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,57 +10,90 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //o scaffold permite dar scroll naa tela para ver o botao quando o teclado aparece
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/skull.png'),
-                //para declarar um asset, precisa ser referenciado la no pubspec.yaml
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                      labelText: 'Email', border: OutlineInputBorder()),
+
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 300,
+              ),
+              //Image.asset('assets/images/skull.png'),
+              //para declarar um asset, precisa ser referenciado la no pubspec.yaml
+              Card(
+                child: Column(
+                  children: [
+                    TextField(
+                      style: TextStyle(color: Colors.black),
+                      cursorColor: Colors.white,
+                      onChanged: (text) {
+                        email = text;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          labelText: 'Email', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      style: TextStyle(color: Colors.black),
+                      cursorColor: Colors.white,
+                      onChanged: (text) {
+                        password = text;
+                      },
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          labelText: 'Senha', border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      labelText: 'Senha', border: OutlineInputBorder()),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'andrei.morais@outlook.com' &&
-                        password == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    }
-                  },
-                  child: const Text('Entrar'),
-                ),
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (email == 'andrei.morais@outlook.com' &&
+                      password == '123') {
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  }
+                },
+                child: const Text('Entrar'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //o scaffold permite dar scroll naa tela para ver o botao quando o teclado aparece
+        body: Stack(
+      //o stack empilha todos os widgets q for colocado no children em ordem
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'assets/images/background.png',
+              fit: BoxFit.cover,
+            )),
+        Container(
+          //esse container foi somente para criar uma mascara mais escura
+          color: Colors.black.withOpacity(0.1),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
