ea5a46cd3cc243b80f3cdcd404fce05eba8f778d
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-15 01:28:25 -0300

    Criada tela de login no tinder

--- lib/login_tinder.dart
+++ lib/login_tinder.dart
@@ -10,31 +10,46 @@ class LoginTinder extends StatefulWidget {
 class _LoginTinderState extends State<LoginTinder> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          body: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.asset(
-                    'assets/images/tinder.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Text('Location changer'),
-                Text('Plugin app for tinder'),
-                FloatingActionButton(
-                  clipBehavior: Clip.hardEdge,
-                  onPressed: () {},
-                ),
-              ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'assets/images/tinder.png',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
-      ],
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 150,
+              ),
+              Text(
+                'Location changer',
+                style: TextStyle(color: Colors.black),
+              ),
+              Container(height: 20),
+              Text(
+                'Plugin app for tinder',
+                style: TextStyle(color: Colors.black),
+              ),
+              Container(
+                height: 100,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: ElevatedButton(
+                  child: Text("Login with Facebook"),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
